859de4c770e81e4d4f0f4fca35323cf70298dbf5
SheillaO <sheilla.alusa@strathmore.edu>
2026-07-26 22:55:23 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,3 +3,18 @@ import { checkAuth, renderGreeting, showHideMenuItems } from "./authUI.js";
 import { getProducts, populateGenreSelect } from "./productService.js";
 import { renderProducts, applySearchFilter } from "./productUI.js";
 import { updateCartIcon } from "./cartService.js";
+
+export let lastRenderedProducts = [];
+
+document.getElementById("logout-btn").addEventListener("click", logout);
+
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".header-menu");
+
+if (toggle && menu) {
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+}
+
+
