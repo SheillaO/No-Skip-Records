8f68daf9fe20a5a7d8a3f66ccdaad75fa6bd3295
SheillaO <sheilla.alusa@strathmore.edu>
2026-07-26 23:11:58 +0300

    Update createTable.js

--- createTable.js
+++ createTable.js
@@ -3,28 +3,22 @@ import { open } from 'sqlite'
 import path from 'node:path'
 
 async function createTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
 
-      const db = await open({
-            filename: path.join('database.db'),
-            driver: sqlite3.Database
-      })
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      artist TEXT NOT NULL,
+      price REAL NOT NULL,
+      image TEXT NOT NULL,
+      year INTEGER,
+      genre TEXT,
+      stock INTEGER
+    )
+  `)
 
-      await db.exec(`
-            CREATE TABLE IF NOT EXISTS products (
-                  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                  title TEXT NOT NULL, 
-                  artist TEXT NOT NULL,
-                  price REAL NOT NULL,
-                  image TEXT NOT NULL,
-                  year INTEGER,
-                  genre TEXT,
-                  stock INTEGER 
-            )
-      
-      `)
-
-      await db.close()
-      console.log('table created')
-}
-
-createTable()
+  
